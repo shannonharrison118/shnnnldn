33ef2c42edc032fd5a29d2f375b212431efb44b7
shannonharrison118 <shannonharrison118@gmail.com>
2023-02-13 21:33:17 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <div id="about">
+        <h1>hi :) im shannon harrison / shnnnldn</h1>
+        <p>shannon harrison is my full name and the name i go by mostly. under this name, i study at the University of Florida
+          studying Computer Science with an interest in web design, frontend development, and ux/ui. i want to further explore
+          how the internet can be used to create art and meaningful experiences online via websites. 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>i also go by shnnn (read shannon) in online spaces. online is where i feel my knowledge in fashion, design, art,
+          and all things creative get to live. shnnn acts slightly as a persona to differentiate the interests and person i 
+          show online vs. shannon who lives everyday life.
+        </p>
+        </div>
+        <div id="small">
+        <p><small>i share a birthday with my melody (jan. 18th), im a capricorn, and i love coffee and tangerine juice.
+        i spend a lot of time on twitter but i should spend that time reading or watching movies. most of my free time though
+        is spent learning about fashion and discovering new designers and scouring the internet for good deals on vintage designer.</small></p>
+        <p><small>as of right now, i hardly have any free time though while i finish up my studies and learn about operating systems
+          , databases, and work in a senior group project focused on drone-human interaction.</small></p>
+          <br></br>
+          <br></br>
+          <br></br>
+      </div>
+      <div id="ascii">
+        <pre>        .-~~-.--.</pre>
+        <pre>       :         {')'}</pre>
+        <pre> .~ ~ -.\       /.- ~~ .</pre>
+        <pre> {'>'}       `.   .'       {'<'}</pre>
+        <pre>(         .- -.         )</pre>
+        <pre> `- -.-~  `- -'  ~-.- -'</pre>
+        <pre>   (        :        )           _ _ .-:</pre>
+        <pre>    ~--.    :    .--~        .-~  .-~  {'}'} </pre>
+        <pre>        ~-.-^-.-~ \_      .~  .-~   .~</pre>
+        <pre>                 \ \'     \ '_ _ -~</pre>
+        <pre>                  `.`.    //</pre>
+        <pre>         . - ~ ~-.__`.`-.//</pre>
+        <pre>     .-~   . - ~  {'}'}~ ~ ~-.~-.</pre>
+        <pre>   .' .-~      .-~       :/~-.~-./:</pre>
+        <pre>  /_~_ _ . - ~                 ~-.~-._</pre>
+        <pre>                                   ~-.{'<'}</pre>
+      </div>
     </div>
   );
 }
