b50b8f40e31a1c4bbf995255b8823caf44e7766a
shannonharrison118 <shannonharrison118@gmail.com>
2023-03-03 12:56:54 -0500

    banner in about

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 import {Navbar} from '../components/Navbar.js';
 import {Footer} from '../components/Navbar.js';
 import me from '../pictures/me.png';
+import banner from '../pictures/banner.png';
 
 function About() {
     return (
@@ -18,36 +19,37 @@ function About() {
                 <div id="aboutcontent">
                     <div id="column">
                         <div id="writing">
-                        <h1>hi :) im shannon harrison / shnnnldn</h1>
-                        <h4>I am a frontend developer and web designer based in New York City</h4>
-                        <br></br>
-                        <p>I am currently a student at the Univeristy of Florida, completing a Bachelor of Science in Computer
-                            Science and graduating in May 2023. I am interested in creating a more beautiful web and finding ways
-                            of being more creative with my internet usage. This has driven me to self-learn frontend development
-                            alongside my studies not only to create my own corners of the internet but make them unique from other 
-                            internet experiences.
-                        </p>
-                        <br></br>
-                        <p>I also go by shnnn (read shannon) in online spaces, where i feel my knowledge in fashion, design, art,
-                            and all things creative get to live. shnnn acts as a persona to differentiate the interests and person i
-                            show online vs. shannon who lives everyday life. Spending time in many online spaces has allowed me to feel
-                            more comfortable in exploring my creativity and the web holds a special place in me.
-                        </p><p>
-                            Existing online in a more creative version of myself is what inspired me to learn more about computer science
-                            and how to create similar enjoyable experiences on the web.
-                        </p>
-                        <br></br>
-                        <div id="small">
-                            <p>fun things about me !!!</p>
-                            <p><small>i share a birthday with my melody (jan. 18th, capricorn), i love sweets, and im obsessed with every cat i see.
-                                fka twigs is also my favorite musical artist and we have the same astrological sun, moon, and venus.
-                                im trying to spend more of my time reading and watching movies but my free time is usually 
-                                spent learning about fashion and discovering new designers.</small></p>
-                            <p><small>as of right now, i am finishing up my studies and learning about operating systems, databases, 
-                                and working in a group project focused on drone-human interaction.</small></p>
+                            <img src={banner} width="100%"></img>
+                            <h1>hi :) im shannon harrison / shnnnldn</h1>
+                            <h4>I am a frontend developer and web designer based in New York City</h4>
                             <br></br>
+                            <p>I am currently a student at the Univeristy of Florida, completing a Bachelor of Science in Computer
+                                Science and graduating in May 2023. I am interested in creating a more beautiful web and finding ways
+                                of being more creative with my internet usage. This has driven me to self-learn frontend development
+                                alongside my studies not only to create my own corners of the internet but make them unique from other
+                                internet experiences.
+                            </p>
                             <br></br>
-                        </div>
+                            <p>I also go by shnnn (read shannon) in online spaces, where i feel my knowledge in fashion, design, art,
+                                and all things creative get to live. shnnn acts as a persona to differentiate the interests and person i
+                                show online vs. shannon who lives everyday life. Spending time in many online spaces has allowed me to feel
+                                more comfortable in exploring my creativity and the web holds a special place in me.
+                            </p><p>
+                                Existing online in a more creative version of myself is what inspired me to learn more about computer science
+                                and how to create similar enjoyable experiences on the web.
+                            </p>
+                            <br></br>
+                            <div id="small">
+                                <p>fun things about me !!!</p>
+                                <p><small>i share a birthday with my melody (jan. 18th, capricorn), i love sweets, and im obsessed with every cat i see.
+                                    fka twigs is also my favorite musical artist and we have the same astrological sun, moon, and venus.
+                                    im trying to spend more of my time reading and watching movies but my free time is usually
+                                    spent learning about fashion and discovering new designers.</small></p>
+                                <p><small>as of right now, i am finishing up my studies and learning about operating systems, databases,
+                                    and working in a group project focused on drone-human interaction.</small></p>
+                                <br></br>
+                                <br></br>
+                            </div>
                         </div>
                     </div>
                     <div id="column">
@@ -68,7 +70,7 @@ function About() {
                             <small>this is what i look like most of the time</small>
                             <br></br>
                             <p><small>currently listening to: </small></p>
-                            <Spotify link = "https://open.spotify.com/playlist/6cTd8aiDx5YxlC9IDhraQh?si=36211e1abde544e4" />
+                            <Spotify link="https://open.spotify.com/playlist/6cTd8aiDx5YxlC9IDhraQh?si=36211e1abde544e4" />
                         </div>
                     </div>
                 </div>
@@ -89,7 +91,6 @@ function About() {
                 </div>
                 <Footer />
             </div>
-            
         </div>
     );
 }
